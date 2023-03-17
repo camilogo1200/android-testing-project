9c5abb77e94da26f9290dc266172f0b66b0c3064
Cristhian Camilo Gómez Narváez <cristhian.gomez@incode.com>
2023-03-17 08:56:34 -0500

    added blink animation to tooltip and behaviour

--- app/src/main/java/com/example/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mytestapplication
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<ImageView>(R.id.expand_me_btn)
+        val tooltipFlashLight = findViewById<ImageView>(R.id.tooltip_flashlight)
 
         val td = TransitionDrawable(
             arrayOf<Drawable>(
@@ -22,15 +27,60 @@ class MainActivity : AppCompatActivity() {
         )
         var isFlashLightOn = false
         btn.setOnClickListener {
-            btn.setImageDrawable(td)
-            if (!isFlashLightOn) {
-                td.startTransition(3000)
-            } else {
-                td.reverseTransition(3000)
-                td.resetTransition()
-            }
+            tooltipFlashLight.isGone = true
+            handleButtonClicked(btn, td, isFlashLightOn)
             isFlashLightOn = !isFlashLightOn
         }
+
+
+        val animatorFadeOut = ObjectAnimator.ofFloat(tooltipFlashLight, "alpha", 1F, 0F).apply {
+            duration = 750
+            start()
+        }
+
+        val animatorFadeIn = ObjectAnimator.ofFloat(tooltipFlashLight, "alpha", 0F, 1F).apply {
+            duration = 750
+            start()
+        }
+
+        animatorFadeIn.duration = 950;
+        animatorFadeIn.repeatCount = 2;
+        animatorFadeIn.repeatMode = ValueAnimator.REVERSE;
+        animatorFadeIn.start()
+//        val blinkAnimation = AnimatorSet().apply {
+//            play(animatorFadeIn).after(animatorFadeOut)
+//            play(animatorFadeIn).before(animatorFadeOut)
+//        }
+//
+//        blinkAnimation.start()
+//        //        AnimatorSet().apply {
+//            play(bou)
+//        }
+
+    }
+
+    private fun handleButtonClicked(
+        btn: ImageView, td: TransitionDrawable, isFlashLightOn: Boolean
+    ) {
+        var isFlashLightOn1 = isFlashLightOn
+        btn.setImageDrawable(td)
+        if (!isFlashLightOn1) {
+            td.startTransition(3000)
+            btn.setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    resources, R.drawable.flash_light_on, null
+                )
+            )
+        } else {
+            td.reverseTransition(3000)
+            td.resetTransition()
+            btn.setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    resources, R.drawable.flashllght_off, null
+                )
+            )
+        }
+
     }
 
 }
